6a06e48786d9c63c766a4931669677c92b6e4fe8
ppuliu <liuhonglei@gmail.com>
2015-04-14 14:36:43 -0700

    viz

--- viz_clusters.py
+++ viz_clusters.py
@@ -13,30 +13,38 @@ def viz_clusters(clusterDir):
 
 	clusters=os.listdir(clusterDir)
 	for d_name in clusters:
+		print d_name 
 		names.append(d_name)
 		d=os.path.join(clusterDir,d_name)
 
 		files=os.listdir(d)
 		fileNum=len(files)
-		m=np.zeros(N,N)		
+		m=np.zeros(shape=(N,N))
+		readNetN=0		
 		for file_name in files:
+			if not file_name.endswith('.cnet'):
+				continue
+			readNetN+=1
 			filepath=os.path.join(d,file_name)
 			with open(filepath) as f:
 				for line in f:
+					if len(line.strip())==0:
+						continue
 					t=line.split('\t')
 					r=int(t[0])-1
 					c=int(t[1])-1
 					m[r,c]+=1.0/fileNum
-		adjmatrix.append(adjmatrix)
+		adjmatrix.append(m)
+		print str(readNetN)+' networks read'
 
 
 	# visualization section
-	figN=math.ceil(math.sqrt(len(names)))
+	figN=int(math.ceil(math.sqrt(len(names))))
 	fig, axes = plt.subplots(figN, figN, figsize=(12, 6),
                          subplot_kw={'xticks': [], 'yticks': []})
 	fig.subplots_adjust(hspace=0.3, wspace=0.05)
 
-	for i in range(names):
+	for i in range(len(names)):
 		ax=axes[i/figN,i%figN]
 		title=names[i]
 		m=adjmatrix[i]
@@ -44,3 +52,10 @@ def viz_clusters(clusterDir):
 		ax.set_title(title)
 
 	plt.show()
+	#plt.savefig('a.pdf')
+
+
+
+if __name__=='__main__':
+	clusterDir=sys.argv[1]
+	viz_clusters(clusterDir)

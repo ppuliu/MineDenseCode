f804bd12e9ff641622a0a79df43a8c6c991f41d0
ppuliu <liuhonglei@gmail.com>
2015-04-14 14:41:00 -0700

    viz files

--- viz_clusters.py
+++ viz_clusters.py
@@ -54,8 +54,48 @@ def viz_clusters(clusterDir):
 	plt.show()
 	#plt.savefig('a.pdf')
 
+def viz_nets(inDir):
 
+	names=[]
+	adjmatrix=[]
+	
+	files=os.listdir(inDir)
+	fileNum=len(files)
+	m=np.zeros(shape=(N,N))
+	readNetN=0		
+	for file_name in files:
+		if not file_name.endswith('.cnet'):
+			continue
+		readNetN+=1
+		filepath=os.path.join(inDir,file_name)
+		with open(filepath) as f:
+			for line in f:
+				if len(line.strip())==0:
+					continue
+				t=line.split('\t')
+				r=int(t[0])-1
+				c=int(t[1])-1
+				m[r,c]+=1.0/fileNum
+	adjmatrix.append(m)
+	print str(readNetN)+' networks read'
+
+
+	# visualization section
+	figN=int(math.ceil(math.sqrt(len(names))))
+	fig, axes = plt.subplots(figN, figN, figsize=(12, 6),
+                         subplot_kw={'xticks': [], 'yticks': []})
+	fig.subplots_adjust(hspace=0.3, wspace=0.05)
+
+	for i in range(len(names)):
+		ax=axes[i/figN,i%figN]
+		title=names[i]
+		m=adjmatrix[i]
+		ax.pcolor(m)
+		ax.set_title(title)
+
+	plt.show()	
 
 if __name__=='__main__':
-	clusterDir=sys.argv[1]
-	viz_clusters(clusterDir)
+	inDir=sys.argv[1]
+	viz_clusters(inDir)
+	viz_nets(inDir)

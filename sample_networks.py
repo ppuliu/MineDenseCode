4908b413f115e41c7996569ec1370a6e8d8f750a
ppuliu <liuhonglei@gmail.com>
2015-04-15 21:08:27 -0700

    add random sampling

--- sample_networks.py
+++ sample_networks.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import shutil
+import random
 
 def sample(inDir,outDir):
 
@@ -23,11 +24,39 @@ def sample(inDir,outDir):
 				shutil.copyfile(in_file,out_file)
 				f.write(file_name.split('.')[0]+'\n')
 
+def random_sample(inDir,outDir,clusterN,sampleN):
+
+	files=os.listdir(inDir)
+
+	if os.path.isdir(outDir):
+		shutil.rmtree(outDir)
+	print "creating "+outDir
+	os.makedirs(outDir)
+
+	for i in range(clusterN):
+		out_dir=os.path.join(outDir,str(i))
+		os.makedirs(out_dir)
+		netidFile=os.path.join(out_dir,'network_ids.txt')
+		with open(netidFile,'w') as f:
+			dic={}
+			for k in range(sampleN):
+				file_n=random.randint(0,len(files)-1)
+				while file_n in dic:
+					file_n=random.randint(0,len(files)-1)
+				dic[file_n]=1
+				file_name=files[file_n]
+				
+				in_file=os.path.join(inDir,file_name)
+				out_file=os.path.join(out_dir,file_name)
+				shutil.copyfile(in_file,out_file)
+				f.write(file_name.split('.')[0]+'\n')
+
+
 if __name__=='__main__':
 	
 	inDir=sys.argv[1]
 	outDir=sys.argv[2]
-	sample(inDir,outDir)
-
+	#sample(inDir,outDir)
+	random_sample(inDir,outDir,500,100)
 
 

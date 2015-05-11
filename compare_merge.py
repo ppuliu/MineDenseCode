6b6e7d675ff975fc017812923f91b8be5c17bd2d
ppuliu <liuhonglei@gmail.com>
2015-05-11 04:20:34 -0700

    add new measuring metric

--- compare_merge.py
+++ compare_merge.py
@@ -89,6 +89,57 @@ def comparePatterns(p1,p2):
 	print 'total number of patterns recalled: {0}'.format(recall)
 	print 'average overlap ratio: {0}'.format(total_ratio/recall)
 
+def readOnePatternNew(file_path):
+	res={}
+	with open(file_path) as f:
+		for line in f:
+			if len(line.strip())==0:
+				continue
+			t=line.split(']')
+			t=t[0][1:].split(',')
+
+			for i in range(len(t)):
+				for j in range(i,len(t)):
+					minNode=min(int(t[i]),int(t[j]))
+					maxNode=max(int(t[i]),int(t[j]))
+					hashCode=str(minNode)+'_'+str(maxNode)
+					if hashCode not in res:
+						res[hashCode]=1
+					else
+						res[hashCode]+=1
+	return res
+
+
+def comparePatternsNew(p1,p2):
+
+	precision=0
+	recall=0
+
+	origionPairNum=len(p1)
+	targetPairNum=len(p2)
+
+	p=0.0
+	for i in p2:
+		p2Count=p2[i]
+		p1Count=0
+		if i in p1:
+			p1Count=p1[i]
+		p+=float(min(p1Count,p2Count))/p2Count
+	precision=p/targetPairNum
+
+	p=0.0
+	for i in p1:
+		p1Count=p1[i]
+		p2Count=0
+		if i in p2:
+			p2Count=p2[i]
+		p+=float(min(p1Count,p2Count))/p1Count
+
+	recall=p/origionPairNum
+
+
+	print 'precision: {0}'.format(precision)
+	print 'recall: {0}'.format(recall)
 
 if __name__=='__main__':
 	param=sys.argv[1]
@@ -109,3 +160,11 @@ if __name__=='__main__':
 		p1=readOnePattern(file1)
 		p2=readOnePattern(file2)
 		comparePatterns(p1,p2)
+
+	if param=='-compareNew':
+		file1=sys.argv[2]
+		file2=sys.argv[3]
+
+		p1=readOnePatternNew(file1)
+		p2=readOnePatternNew(file2)
+		comparePatternsNew(p1,p2)
